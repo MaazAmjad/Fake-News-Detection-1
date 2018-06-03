5d4b9de9f2fd018cd9875a3522ddb21e87508943
DominicChai <38520006+DominicChai@users.noreply.github.com>
2018-06-03 16:42:22 +1000

    Update Determine_LSTM_input_tensor_size.py

--- FNC/Determine_LSTM_input_tensor_size.py
+++ FNC/Determine_LSTM_input_tensor_size.py
@@ -6,9 +6,6 @@ import nltk
 from nltk.tokenize import word_tokenize
 import numpy as np
 
-#1.word vector 一个一个的feed into LSTM，使用padding固定序列数据长度
-#2.LDA 为每一个文章 生成固定长度的描述topic的关键词 每个关键词当然对应着一个word vector
-#3.为每个document生成一个固定长度doc vector Doc2vec是不是和Word2Vec一样语料库越多越精确？
 
 trainBody = pd.read_csv('train_bodies.csv') #length is 1683
 
@@ -26,7 +23,7 @@ for index in range(trainBody['articleBody'].shape[0]):
     lengthList = np.append(lengthList,len(word_tokenize(line)))
 
 print "the average length of text body is: "
-print int(np.mean(lengthList)) #424 without 标点符号
+print int(np.mean(lengthList)) #424 
 
 averageLength = np.mean(lengthList)
 #trainBody.to_csv('train_bodies.csv')
